084220d538e570c32ea95b15f3a18ea91ccab130
Nahyun-Kang <tarea202001@gmail.com>
2024-04-26 22:14:58 +0900

    Chore: 절대경로 설정을 위한 jsconfig.json 파일 생성 및 옵션 추가

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
-import '../styles/globals.css';
-import '../styles/reset.css';
+import '@/styles/globals.css';
+import '@/styles/reset.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
